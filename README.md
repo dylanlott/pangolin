c6f8bd74bcd56ec4050577faca20144289d7cef0
Dylan Lott <lott.dylan@gmail.com>
2018-04-03 17:43:03 -0600

    WIP implementing add to tree functionality

--- README.md
+++ README.md
@@ -14,6 +14,14 @@ Pangolin is a database written in Go.
 - [ ] Serve a simple GUI
 - [ ] CLI for running the database
 
+## API 
+GET `/` will return an overview of the database 
+
+An example response would look like
+
+```JSON
+  {"version":"0.0.1","buckets":["none"]}
+```
 
 ## Testing 
 Install dependencies, then you can run 
@@ -30,7 +38,8 @@ Pangolin is a project written for an internal hackathon at Storj Labs.
 Indexing is accomplished using a left-leaning red-black tree.
 This achieves log2N searches and 2 log2N tree height.
 
-https://en.wikipedia.org/wiki/Left-leaning_red%E2%80%93black_tree
+- [https://en.wikipedia.org/wiki/Left-leaning_red%E2%80%93black_tree]()
+- [http://www.cs.princeton.edu/~rs/talks/LLRB/LLRB.pdf]()
 
 ## License 
 Pangolin is AGPL 3.0 Licensed
--- main.go
+++ main.go
@@ -12,21 +12,39 @@ import (
 
 const file = "/tmp/pangolin.db"
 
+type Blob struct {
+	data string `json:"data"`
+	id int `json:"id"`
+}
+
+// type Blob interface {
+// 	Less(than Item) bool
+// }
+
+// func (b *Blob) Less (than llrb.Item) bool {
+// 	switch than {
+// 	case nil: 
+// 		return false
+// 	default:
+// 		return b.id < Blob(than).id
+// 	}
+// }
+
+// func lessInt(a, b interface{}) bool { return a.(int) < b.(int) }
+
 func main() {
 	fmt.Println("pangolin is starting up")
 
  	http.HandleFunc("/", getSpec)
 
-	tree := llrb.New() 
-	fmt.Println(tree)
-	fmt.Println(tree.Len())
+	tree := CreateTree() 
 
 	writeErr := Save(file, tree)
 	Check(writeErr)
 
   if err := http.ListenAndServe(":8080", nil); err != nil {
     panic(err)
-  } 
+  }
 }
 
 func Check(e error) {
@@ -42,9 +60,17 @@ type spec struct {
 	Buckets []string `json:"buckets"`
 }
 
+func getBuckets () string {
+	return "none"
+}
+
 func getSpec (w http.ResponseWriter, r *http.Request) {
-	formattedJson := map[string]string{"version": "0.0.1", "buckets": "none"}
-	message, _ := json.Marshal(formattedJson)
+	response := &spec{
+		Version: "0.0.1",
+		Buckets: []string{getBuckets()},
+	}
+
+	message, _ := json.Marshal(response)
   w.Write([]byte(message))
 }
 
@@ -58,7 +84,7 @@ func Save (path string, object interface{}) error {
 	return err
 }
 
-func Load(path string, object interface{}) error {
+func Load (path string, object interface{}) error {
 	file, err := os.Open(path)
 	if err == nil {
 		decoder := gob.NewDecoder(file)
@@ -66,4 +92,9 @@ func Load(path string, object interface{}) error {
 	}
 	file.Close()
 	return err
+}
+
+func CreateTree () *llrb.LLRB {
+	tree := llrb.New()
+	return tree
 }
