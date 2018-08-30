5b4c6f24cb9272b532869dbb96037bb20bc55174
Dylan Lott <lott.dylan@gmail.com>
2018-08-30 15:53:44 -0700

    add uuid generator for ids

--- database/persist.go
+++ database/persist.go
@@ -6,6 +6,8 @@ import (
 	"sync"
 	"encoding/json"
 	"bytes"
+	"fmt"
+	"crypto/rand"
 )
 
 var lock sync.Mutex
@@ -59,3 +61,16 @@ var Unmarshal = func(r io.Reader, v interface{}) error {
   return json.NewDecoder(r).Decode(v)
 }
 
+// Creates a new UUID as a unique identifier for each item 
+func newUUID() (string, error) {
+	uuid := make([]byte, 16)
+	n, err := io.ReadFull(rand.Reader, uuid)
+	if n != len(uuid) || err != nil {
+		return "", err
+	}
+	// variant bits; see section 4.1.1
+	uuid[8] = uuid[8]&^0xc0 | 0x80
+	// version 4 (pseudo-random); see section 4.1.3
+	uuid[6] = uuid[6]&^0xf0 | 0x40
+	return fmt.Sprintf("%x-%x-%x-%x-%x", uuid[0:4], uuid[4:6], uuid[6:8], uuid[8:10], uuid[10:]), nil
+}
